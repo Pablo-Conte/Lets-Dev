7233f5559f71d74c9e05c581c99839d047aa3281
PabloConte <pablo.correa.nr@gmail.com>
2022-06-14 22:56:51 -0300

    Primeira Estrutura Criada

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,59 @@
 import React from 'react'
+import Titulo from './componentes/Titulo'
 
-function App() {
+
+function MeuSite() {
+
+  // var titulo: string = "Olá mundo!"    -> usa-se como variável dentro do return do App, utilizando "{var_aqui}"
+  // ou
+  // var titulo: string = "Olá mundo!"
+  
+  /*function renderizarTitulo(){
+    return titulo + " Seja Bem-vindo(a)" //retorna o conteúdo da variável titulo criada anteriormente + o que quisermos
+  } */  // usa-se {nomefunção()}
+
+
+  function minhaFuncao(){
+    alert("Opá!")
+  }
+
+  const disparaAlert = (label: string) => {
+    alert(label)
+  }
+
+  function MeuBotao(props: any){
+    //const label = props.label            -> essa e a sintaxe de baixo são iguais
+
+    const {label} = props;
+
+    return(
+      <button onClick={() => disparaAlert(label)}>
+        {label}
+      </button>
+    )
+  }
 
   return (
     <div>
-      <h1>Olá Mundo!</h1>
+      <Titulo titulo="Home"/>
+
+      <MeuBotao label="Botão 1"/>
+      <MeuBotao label="Botão 2"/>
     </div>
 
   );
 }
 
-export default App;
+export default MeuSite;
+
+
+
+
+
+//1° letra maiúsculo quer dizer que tu cria um componente.
+//2° letra minúscula quer dizer que tu cria uma função normal que vai retornar algo.
+//class = className
+//onclick = onClick
+//funcao(){ } = const funcao = () => { }
+//function MeuComponente(){ return <div/>} 
+//function MeuComponente = () => {return <div/>}
